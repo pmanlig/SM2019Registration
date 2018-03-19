02bd004440de1dd78d4c9cc3cc7bf7ec181bc55c
Patrik Manlig <patrik@manlig.org>
2018-03-19 21:56:45 +0100

    Vertical headers

--- src/RegistrationHeader.js
+++ src/RegistrationHeader.js
@@ -5,9 +5,10 @@ export class RegistrationHeader extends Component {
 		const majorHeaders = [];
 		const minorHeaders = [];
 		this.props.columns.forEach((column) => {
-			majorHeaders.push(<th key={majorHeaders.length} colSpan={column.subfields.length}>{column.name}</th>);
+			majorHeaders.push(<th key={majorHeaders.length} class="major" colSpan={column.subfields.length}>{column.name}</th>);
+			var classes = column.name === "Skytt" ? "minor" : "minor vert";
 			column.subfields.forEach((minor) => {
-				minorHeaders.push(<th key={minorHeaders.length} width={minor.width}>{minor.name}</th>);
+				minorHeaders.push(<th key={minorHeaders.length} class={classes}><div>{minor.name}</div></th>);
 			});
 		});
 		return (
