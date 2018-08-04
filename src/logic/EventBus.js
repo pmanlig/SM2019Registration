ed9410375d65e7d71e22a9ff60630c33f9b55d4a
Patrik Manlig <patrik@manlig.org>
2018-08-04 10:06:36 +0200

    New implementation of event handling

--- src/logic/EventBus.js
+++ src/logic/EventBus.js
@@ -27,6 +27,44 @@ export class EventBus {
 		this.bus = new Subject();
 	}
 
+	manageEvents(target) {
+		// Handlers
+		target._handlers = [];
+
+		// Subscribe
+		let targetSubscribe = function (e, h) { this._handlers.push({ event: e, handler: h, subscription: null }); }
+		target.subscribe = targetSubscribe.bind(target);
+
+		// Fire
+		target.fire = this.fire.bind(this);
+
+		// ComponentDidMount
+		let mySubscribe = this.subscribe.bind(this);
+		let inheritedMount = target.componentDidMount;
+		let targetMount = function () {
+			this._handlers.forEach(h => h.subscription = mySubscribe(h.event, h.handler));
+			if (inheritedMount) {
+				inheritedMount.apply(target);
+			}
+		}
+		target.componentDidMount = targetMount.bind(target);
+
+		// ComponentWillUnmount
+		let inheritedUnmount = target.componentWillUnmount;
+		let targetUnmount = function () {
+			this._handlers.forEach(h => {
+				if (h.subscription) {
+					h.subscription.unsubscribe();
+					h.subscription = null;
+				}
+			});
+			if (inheritedUnmount) {
+				inheritedUnmount.apply(target);
+			}
+		}
+		target.componentWillUnmount = targetUnmount.bind(target);
+	}
+
 	subscribe(event, action) {
 		if (event === undefined) {
 			throw new Error("Cannot subscribe to undefined event!");
