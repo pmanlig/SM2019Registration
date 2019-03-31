daf257586d0cc5d8748f923c88b24e3461b2779e
Patrik Manlig <patrik@manlig.org>
2019-03-31 13:23:56 +0200

    Configurable Q&A

--- src/views/HelpView.js
+++ src/views/HelpView.js
@@ -4,24 +4,39 @@ export class HelpView extends React.Component {
 	static register = { name: "HelpView" }
 	static wire = ["Events", "EventBus"];
 
+	constructor(props) {
+		super(props);
+		this.state = { qa: [] }
+		fetch('/qa.json')
+			.then(res => res.json())
+			.then(json => this.setState({ qa: json }));
+	}
+
 	componentWillMount() {
 		this.EventBus.fire(this.Events.changeTitle, "Hjälp?");
 	}
 
+	link(link) {
+		if (!link.includes("~")) return link;
+		let parts = link.split("~")
+		return <a href={parts[0]}>{parts[1]}</a>;
+	}
+
+	linkify(text) {
+		let parts = text.split("$").map(t => this.link(t));
+		return <p>{parts.map((p, index) => <span key={index}>{p}</span>)}</p>;
+	}
+
 	render() {
+		let qaId = 1;
 		return <div className="content">
 			<p>Vi gör vårt bästa för att alla funktioner i systemet ska vara intuitiva och självförklarande,
 				men om vi misslyckas med det kan du hitta svar på de vanligaste frågorna på den här sidan.</p>
 			<p>Hittar du inte svaret på det du undrar över? Skicka ett mail till <a href="mailto:anmalan@xkretsen.se">anmalan@xkretsen.se</a></p>
-			<h3>Hur hittar jag tävlingar?</h3>
-			<p>Klicka på knappen "tävlingar", eller på logotypen i övre vänstra hörnet.</p>
-			<h3>Kommer ni att mobilanpassa sidan?</h3>
-			<p>Vi planerar att göra det när vi får tid. Tyvärr kan vi inte lova att vi kommer att ha tid att göra det innan SM 2019;
-				det är helt enkelt för mycket jobb för att vi ska kunna garantera det.</p>
-			<h3>Sidan fungerar inte i Internet Explorer?</h3>
-			<p>Tyvärr inte. Internet Explorer är vid det här laget en gammal webbläsare som tyvärr inte stödjer
-				de senaste standarderna för webbutveckling. Vi kan lägga till stöd för IE, men det innebär extra
-				jobb som vi inte har haft tid med än. Tills vidare rekommenderar vi att du använder <a href="https://www.google.com/chrome/">Chrome</a>.</p>
+			{this.state.qa.map(qa => <div key={qaId++}>
+				<h3>{qa.q}</h3>
+				{this.linkify(qa.a)}
+			</div>)}
 		</div>;
 	}
 }
